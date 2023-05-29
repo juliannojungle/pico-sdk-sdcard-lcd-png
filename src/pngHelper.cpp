99caa2e4d2799dd997154fb8beba06dcca066843
Julianno F. C. Silva <juliannojungle@gmail.com>
2023-05-29 09:28:11 -0300

    :sparkles: color png done

--- src/pngHelper.cpp
+++ src/pngHelper.cpp
@@ -70,18 +70,6 @@ void DisplayPng(FIL &file) {
     png_get_IHDR(png_ptr, info_ptr, &width, &height, &bit_depth, &color_type, &interlace_type, NULL, NULL);
     printf("PNG info: width: %d, height: %d, bit_depth: %d\n", width, height, bit_depth);
 
-    if (color_type == PNG_COLOR_TYPE_GRAY) printf("PNG_COLOR_TYPE_GRAY\n");
-    if (color_type == PNG_COLOR_TYPE_GRAY_ALPHA) printf("PNG_COLOR_TYPE_GRAY_ALPHA\n");
-    if (color_type == PNG_COLOR_TYPE_PALETTE) printf("PNG_COLOR_TYPE_PALETTE\n");
-    if (color_type == PNG_COLOR_TYPE_RGB) printf("PNG_COLOR_TYPE_RGB\n");
-    if (color_type == PNG_COLOR_TYPE_RGB_ALPHA) printf("PNG_COLOR_TYPE_RGB_ALPHA\n");
-
-    /* Expand paletted colors into true RGB triplets. */
-    if (color_type == PNG_COLOR_TYPE_PALETTE) {
-        png_set_palette_to_rgb(png_ptr);
-        png_read_update_info(png_ptr, info_ptr);
-    }
-
     printf("Initialize display...\n");
     if (DEV_Module_Init() != 0) {
         png_destroy_read_struct(&png_ptr, &info_ptr, NULL);
@@ -102,9 +90,12 @@ void DisplayPng(FIL &file) {
     DEV_Digital_Write(EPD_DC_PIN, 1);
     DEV_Digital_Write(EPD_CS_PIN, 0);
 
-    png_color_8 *sig_bit;
-    png_get_sBIT(png_ptr, info_ptr, &sig_bit);
-    printf("red: %d, green: %d, blue: %d, gray: %d, alpha: %d\n", sig_bit->red, sig_bit->green, sig_bit->blue, sig_bit->gray, sig_bit->alpha);
+    int num_palette = 0;
+    png_colorp palette = NULL;
+
+    if (color_type == PNG_COLOR_TYPE_PALETTE) {
+        png_get_PLTE(png_ptr, info_ptr, &palette, &num_palette);
+    }
 
     for (row = 0; row < maxRow; row++) {
         row_pointers = (png_bytep)png_malloc(png_ptr, png_get_rowbytes(png_ptr, info_ptr));
@@ -112,10 +103,20 @@ void DisplayPng(FIL &file) {
 
         if (row == 0) printf("rowbytes: %d\n", png_get_rowbytes(png_ptr, info_ptr));
 
-        for (col = 0; col < maxCol * 3; col += 3) {
-            png_byte red = row_pointers[col];
-            png_byte green = row_pointers[col+1];
-            png_byte blue = row_pointers[col+2];
+        for (col = 0; col < maxCol; col++) {
+            png_byte red, green, blue;
+
+            if ((color_type == PNG_COLOR_TYPE_PALETTE) && (palette != NULL)) {
+                red = palette[row_pointers[col]].red;
+                green = palette[row_pointers[col]].green;
+                blue = palette[row_pointers[col]].blue;
+            } else {
+                // if the image is paletted but we don't have a palette, display as grayscale using palette index.
+                png_bytep pixel = &row_pointers[col];
+                red = *(pixel++);
+                green = *(pixel++);
+                blue = *(pixel++);
+            }
 
             /* The LCD uses RGB565 16-bits format: RRRRRGGG GGGBBBBB */
             DEV_SPI_WriteByte((red & 0b11111000) | ((green & 0b11100000) >> 5));
